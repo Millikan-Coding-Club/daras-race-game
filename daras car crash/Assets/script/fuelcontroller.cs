d21fa9bd1192819768fdcb13c9e60325bdba0c30
daramut <daramutyt@gmail.com>
2023-10-20 20:36:31 -0700

    update9

---
+++ daras car crash/Assets/script/fuelcontroller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fuelcontroller : MonoBehaviour
+{
+    public static fuelcontroller instance;
+
+    [SerializeField] private Image _fuelImage;
+    [SerializeField, Range(0.1f, 5f)] private float _fuelDrainSpeed = 1f;
+    [SerializeField] private float _maxFuelAmount = 100f;
+    [SerializeField] private Gradient _fuelGradient;
+
+    private float _currentFuelAmount;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        _currentFuelAmount = _maxFuelAmount;
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        _currentFuelAmount -= Time.deltaTime * _fuelDrainSpeed;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        _fuelImage.fillAmount = (_currentFuelAmount / _maxFuelAmount);
+        _fuelImage.color = _fuelGradient.Evaluate(_fuelImage.fillAmount);
+    }
+
+}
